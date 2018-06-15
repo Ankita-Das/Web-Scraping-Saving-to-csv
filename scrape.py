b97faca022e18748357f9effb42f470fa187dc35
Ankita Das <ankidas44@gmail.com>
2018-06-15 08:37:26 +0530

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,22 +1,24 @@
+
 import urllib.request
 import datetime
 from bs4 import BeautifulSoup
 import csv
+from scrape2 import cake_desp
 quote_page = "https://www.bakingo.com/cake-delivery"
 page = urllib.request.urlopen(quote_page)
 soup = BeautifulSoup(page, "html.parser")
 
 csv_file=open('scraped.csv','w')
 csv_writer=csv.writer(csv_file)
-csv_writer.writerow(['Product_ID','links','title','short_title','price','description','Date_Time'])
+csv_writer.writerow(['Product_ID','Links','Title','Short_title','Price','Introducing','Date_Time','Description'])
 
 img_list=[]
 tit_list=[]
 s_tit_list=[]
 p_list=[]
-des_list=[]
+intro_list=[]
 pid=[]
-
+desc_list=[]
 
 
 print('**************************************************')
@@ -27,9 +29,8 @@ print('**************************************************')
 
 for b in soup.find_all("div",{'class':'cake-img'}):
     for a in b.find_all('img',src=True):
-        print ("Found the URL:", a['src'])
+        #print ("Found the URL:", a['src'])
         links=a['src']
-        
         #csv_writer.writerow([links])
         img_list=img_list+[links]
 
@@ -57,13 +58,19 @@ for f in soup.find_all("div",{'class':'cake-p'}):
     p_list=p_list+[price]
 dttm=[]
 for g in soup.find_all("div",{'class':'cake-desc'}):
-    #print ("Found the URL:",b.text)
-    des=g.text.strip()
+    #print ("Found the URL to move:",b.text)
+    intro=g.text.strip()
     #csv_writer.writerow([links])
-    des_list=des_list+[des]
+    intro_list=intro_list+[intro]
     dttm=dttm+[datetime.datetime.now()]
 
-
+for i in soup.find_all("div",{'class':'cake-title'}):
+    for j in i.find_all('a',href=True):
+        url="https://www.bakingo.com"
+        url=url + j['href']
+        #print("Linked to page:"+ url+"\n")
+        desc=cake_desp(url)
+        desc_list=desc_list+[desc]
 
 
 
@@ -71,9 +78,10 @@ for g in soup.find_all("div",{'class':'cake-desc'}):
     dttm=dttm+[datetime.datetime.now()];'''
     
 
-zipped=zip(pid,img_list,tit_list,s_tit_list,p_list,des_list,dttm)
+zipped=zip(pid,img_list,tit_list,s_tit_list,p_list,intro_list,dttm,desc_list)
 d=list(zipped)#list of lists
-print(d)
+#print(d)
 csv_writer.writerows(d)
 csv_file.close()
+
 
